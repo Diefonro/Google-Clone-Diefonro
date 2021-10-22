063edd44cf8189f1c1a0db5cc324bd79c8f3fcbd
Diefonro <dfonsecar@unal.edu.co>
2021-10-22 02:32:56 -0500

    made the dd images clickable

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
-
 function clickSearch(){
     window.location = "https://google.com";
+}
+
+function clickMaps(){
+    window.location = "https://maps.google.com";
+}
+
+function clickKeep(){
+    window.location = "https://keep.google.com";
+}
+
+function clickDrive(){
+    window.location = "https://drive.google.com";
+}
+
+function clickCalendar(){
+    window.location = "https://calendar.google.com";
+}
+
+function clickPhotos(){
+    window.location = "https://photos.google.com";
 }
