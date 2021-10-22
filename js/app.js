51b5ddef743e94755d11257a4e1874dbafcc4735
Diefonro <dfonsecar@unal.edu.co>
2021-10-22 02:42:04 -0500

    test

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
-function clickSearch(){
-    window.location = "https://google.com";
+function clickSearch() {
+  window.location = "https://google.com";
 }
 
-function clickMaps(){
-    window.location = "https://maps.google.com";
+function clickMaps() {
+  window.location = "https://maps.google.com";
 }
 
-function clickKeep(){
-    window.location = "https://keep.google.com";
+function clickKeep() {
+  window.location = "https://keep.google.com";
 }
 
-function clickDrive(){
-    window.location = "https://drive.google.com";
+function clickDrive() {
+  window.location = "https://drive.google.com";
 }
 
-function clickCalendar(){
-    window.location = "https://calendar.google.com";
+function clickCalendar() {
+  window.location = "https://calendar.google.com";
 }
 
-function clickPhotos(){
-    window.location = "https://photos.google.com";
+function clickPhotos() {
+  window.location = "https://photos.google.com";
 }
 
-function clickAccount(){
-    window.location = "https://myaccount.google.com";
+function clickAccount() {
+  window.location = "https://myaccount.google.com";
 }
